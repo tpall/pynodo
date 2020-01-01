e030ef8f3bc4dbda87ff9fba2be0d1b9b8d57981
Taavi Päll <tapa741@gmail.com>
2020-01-01 14:48:16 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zenhelper',
+      version='0.1',
+      description='Python wrapper for Zenodo API.',
+      url='http://github.com/tpall/zenhelper',
+      author='Taavi Päll',
+      author_email='tapa741@gmail.com',
+      license='MIT',
+      packages=['zenhelper'],
+      zip_safe=False)
