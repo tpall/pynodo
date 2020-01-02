25cec72ccd155d81353ea3358b0fb1852a377c22
Taavi Päll <tapa741@gmail.com>
2020-01-02 15:27:33 +0200

    rename tests

--- tests/test_depositions.py
+++ tests/test_depositions.py
@@ -2,23 +2,38 @@ import zenhelper
 import os
 import json
 
+# Create zenodo (sandbox) instance 
 zen = zenhelper.depositions(access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True)
+
+# List user depositions
 depos = zen.list(params={"size": 20})
 for d in depos:
     print("title:{}, id: {}".format(d["title"], d["id"]))
     zen.delete(d["id"])
 
+# Create a new deposition with some metadata
 data = {"metadata": {"title": "My first upload", "upload_type": "poster", "description": "This is my first upload", "creators": [{"name": "Päll, Taavi", "affiliation": "UT"}]}}
 new_depo = zen.create(data=data)
 print(new_depo)
 
+# Should fail. Try to retrieve deposition info without deposition id
 ret_depo = zen.retrieve()
 print(ret_depo)
 
+# Retrieve deposition info
 ret_depo = zen.retrieve(deposition=new_depo["id"])
 print(ret_depo)
 
+# Update deposition metadata
 updates = {"metadata": {"title": "Modified upload", "upload_type": "dataset", "description": "This is updated upload", "creators": [{"name": "Päll, Taavi", "affiliation": "UT"}, {"name": "Sus, Scrofa", "affiliation": "Mets"}]}}
 updated_depo = zen.update(deposition=new_depo["id"], data=updates)
 print(updated_depo)
 
+zen_id = zenhelper.depositions(access_token=os.environ["ZENODO_SANDBOX_PAT"], deposition=new_depo["id"], sandbox=True)
+print(zen_id.list())
+
+id_new_depo = zen_id.create(data=data)
+print(id_new_depo)
+
+id_ret_depo = zen_id.retrieve()
+print(id_ret_depo)
