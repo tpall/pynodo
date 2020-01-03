5a44370c7290ffa63e67ddd42fd087be3ff930fb
Taavi Päll <tapa741@gmail.com>
2020-01-03 20:38:30 +0200

    updated depostitions tests

--- tests/test_depositions.py
+++ tests/test_depositions.py
@@ -6,26 +6,27 @@ import pytest
 # Should fail
 def test_zenapi_zenodo():
     with pytest.raises(Exception):
-        zenapi.Zenodo(
-        access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True
-    )
+        zenapi.Zenodo(access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True)
+
 
 # Test creating zenodo (sandbox) instance
 @pytest.fixture
 def depositions_object():
-    return zenapi.Depositions(access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True)
+    return zenapi.Depositions(
+        access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True
+    )
 
 
 # Create a new deposition with some metadata
 def test_depositions_create(depositions_object):
     data = {
-    "metadata": {
-        "title": "Test",
-        "upload_type": "poster",
-        "description": "This is test upload",
-        "creators": [{"name": "Sus, scofa", "affiliation": "Forest Inc."}],
+        "metadata": {
+            "title": "Test",
+            "upload_type": "poster",
+            "description": "This is test upload",
+            "creators": [{"name": "Sus, scofa", "affiliation": "Forest Inc."}],
         }
-        }
+    }
     resp = depositions_object.create(data=data)
     assert "Test" in resp
 
@@ -40,34 +41,37 @@ def test_depositions_list(depositions_object):
 @pytest.fixture
 def test_deposition(depositions_object):
     data = {
-    "metadata": {
-        "title": "Test",
-        "upload_type": "poster",
-        "description": "This is test upload",
-        "creators": [{"name": "Sus, scofa", "affiliation": "Forest Inc."}],
+        "metadata": {
+            "title": "Test",
+            "upload_type": "poster",
+            "description": "This is test upload",
+            "creators": [{"name": "Sus, scofa", "affiliation": "Forest Inc."}],
         }
-        }
+    }
     return depositions_object.create(data=data)
 
+
+# Testing deposition delete
 def test_depositions_delete(depositions_object, test_deposition):
     resp = depositions_object.delete(test_deposition.id)
     assert resp == 204
 
 
-# Retrieve deposition info
+# Testing deposition retrieve
 def test_depositions_retrieve(depositions_object, test_deposition):
     resp = depositions_object.retrieve(test_deposition.id)
+    assert resp["id"] == test_deposition.id
 
-# # Update deposition metadata
-# updates = {
-#     "metadata": {
-#         "title": "Modified upload",
-#         "upload_type": "dataset",
-#         "description": "This is updated upload",
-#         "creators": [
-#             {"name": "Päll, Taavi", "affiliation": "UT"},
-#             {"name": "Sus, Scrofa", "affiliation": "Mets"},
-#         ],
-#     }
-# }
-# updated_depo = zen.update(deposition=new_depo.id, data=updates)
+
+# Update deposition metadata
+def test_depositions_update(depositions_object, test_deposition):
+    updates = {
+        "metadata": {
+            "title": "Modified upload",
+            "upload_type": "dataset",
+            "description": "This is updated upload",
+            "creators": [{"name": "Felis, Catus", "affiliation": "Sofa"},],
+        }
+    }
+    resp = depositions_object.update(deposition=test_deposition.id, data=updates)
+    assert resp["title"] == "Modified upload"
