62098981dfdb19a7ec390fcae196074ac0fdc2e0
Taavi Päll <tapa741@gmail.com>
2020-01-04 15:32:57 +0200

    updated tests

--- tests/test_depositions.py
+++ tests/test_depositions.py
@@ -1,43 +1,24 @@
 import zenapi
 import os
 import pytest
+import hashlib
 
-# Should fail
+# Should fail: calling base class
 def test_zenapi_zenodo():
     with pytest.raises(Exception):
         zenapi.Zenodo(access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True)
 
 
-# Test creating zenodo (sandbox) instance
-@pytest.fixture
+# Creating zenodo (sandbox) depositions instance for tests
+@pytest.fixture(scope="module")
 def depositions_object():
     return zenapi.Depositions(
         access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True
     )
 
 
-# Create a new deposition with some metadata
-def test_depositions_create(depositions_object):
-    data = {
-        "metadata": {
-            "title": "Test",
-            "upload_type": "poster",
-            "description": "This is test upload",
-            "creators": [{"name": "Sus, scofa", "affiliation": "Forest Inc."}],
-        }
-    }
-    resp = depositions_object.create(data=data)
-    assert "Test" in resp
-
-
-# List user depositions
-def test_depositions_list(depositions_object):
-    resp = depositions_object.list(params={"size": 1})
-    assert len(resp) == 1
-
-
 # Create test deposition
-@pytest.fixture
+@pytest.fixture(scope="module")
 def test_deposition(depositions_object):
     data = {
         "metadata": {
@@ -50,10 +31,16 @@ def test_deposition(depositions_object):
     return depositions_object.create(data=data)
 
 
-# Testing deposition delete
-def test_depositions_delete(depositions_object, test_deposition):
-    resp = depositions_object.delete(test_deposition.id)
-    assert resp == 204
+# Create a new deposition with some metadata
+def test_depositions_create(test_deposition):
+    resp = test_deposition
+    assert "Test" in resp
+
+
+# List user depositions
+def test_depositions_list(depositions_object):
+    resp = depositions_object.list(params={"size": 1})
+    assert len(resp) == 1
 
 
 # Testing deposition retrieve
@@ -74,3 +61,41 @@ def test_depositions_update(depositions_object, test_deposition):
     }
     resp = depositions_object.update(deposition=test_deposition.id, data=updates)
     assert resp["title"] == "Modified upload"
+
+# Create new instance for listing files
+@pytest.fixture(scope="module")
+def deposition_files_object(test_deposition):
+    return zenapi.DepositionFiles(
+        deposition=test_deposition.id, access_token=os.environ["ZENODO_SANDBOX_PAT"], sandbox=True
+    )
+
+LOCAL = "tests/upload.txt"
+UPLOADED = "uploaded_file.txt"
+DOWNLOADED = "tests/uploaded_file.txt"
+
+def md5(file):
+    local_md5 = hashlib.md5()
+    with open(file, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            local_md5.update(chunk)
+    return local_md5.hexdigest()
+
+# Upload file
+def test_file_upload(deposition_files_object):
+    deposition_files_object.upload(LOCAL, UPLOADED)
+    assert deposition_files_object.files[UPLOADED].checksum == md5(LOCAL)
+
+# Download file
+def test_file_download(deposition_files_object):
+    deposition_files_object.download(UPLOADED, "tests")
+    assert deposition_files_object.files[UPLOADED].checksum == md5(DOWNLOADED)
+
+# Testing file delete
+def test_file_delete(deposition_files_object):
+    resp = deposition_files_object.delete(UPLOADED)
+    assert resp == 204
+
+# Testing deposition delete
+def test_depositions_delete(depositions_object, test_deposition):
+    resp = depositions_object.delete(test_deposition.id)
+    assert resp == 204
